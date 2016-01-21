c0b407c294e525c79b87f860f655e374c28cd36e
mark <drsparki@gmail.com>
2016-01-21 08:42:14 -0500

    modify setup to use data_files

--- setup.py
+++ setup.py
@@ -17,12 +17,14 @@ from setuptools import setup
 
 setup(name='hillmaker',
       version='0.1.0',
-      description='Python based occupancy analysis tool',
+      description='a Python based occupancy analysis tool',
       author='Mark Isken',
       author_email='isken@oakland.edu',
       url='http://github.com/misken/hillmaker/',
+      #include_package_data = True,
       packages=['hillmaker'],
-      package_data={'hillmaker': ['data/*.csv']},
+      #package_data={'hillmaker': ['data/*.csv']},
+      data_files=[('sample_data', ['data/*.csv'])],
       platforms='any',
       classifiers = [
         'Programming Language :: Python :: 3 :: Only',
