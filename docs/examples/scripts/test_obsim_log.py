2189294618c2fddd0e77c76709e1a574119d7a31
misken <isken@oakland.edu>
2022-07-13 20:03:54 -0400

    reorged docs and examples

---
+++ docs/examples/scripts/test_obsim_log.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+import hillmaker as hm
+
+# Read input data into DataFrame
+file_stopdata = '../data/unit_stop_log.csv'
+df = pd.read_csv(file_stopdata)
+
+# Convert simulation clock times to pandas Timestamps as offsets from a base datetime
+basedate = pd.Timestamp('20150215 00:00:00')
+
+df['EnteredTS'] = df.apply(lambda row:
+                           pd.Timestamp(round((basedate + pd.DateOffset(hours=row['Entered'])).value, -9)), axis=1)
+
+df['ExitedTS'] = df.apply(lambda row:
+                           pd.Timestamp(round((basedate + pd.DateOffset(hours=row['Exited'])).value,-9)), axis=1)
+                           
+                           scenario_name = 'log_unitocc_test'
+
+in_fld_name = 'EnteredTS'
+out_fld_name = 'ExitedTS'
+cat_fld_name = 'Unit'
+start_analysis = '4/1/2015'
+end_analysis = '11/1/2017'
+
+# Optional inputs
+bin_size_mins = 120
+output_path = './output'
+
+hm.make_hills(scenario_name, df, in_fld_name, out_fld_name,
+                     start_analysis, end_analysis, cat_fld_name,
+                     bin_size_minutes=bin_size_mins,
+                     output_path=output_path, verbose=1)
---
+++ docs/examples/scripts/test_shortstay.py
@@ -0,0 +1,27 @@
+
+
+import pandas as pd
+import hillmaker as hm
+
+file_stopdata = '../data/ShortStay.csv'
+
+# Required inputs
+scenario = 'sstest_60'
+in_fld_name = 'InRoomTS'
+out_fld_name = 'OutRoomTS'
+cat_fld_name = 'PatType'
+start = '1/1/1996'
+end = '3/30/1996'
+
+# Optional inputs
+bin_mins = 60
+output_path = './output'
+
+
+df = pd.read_csv(file_stopdata, parse_dates=[in_fld_name, out_fld_name])
+
+hm.make_hills(scenario, df, in_fld_name, out_fld_name,
+                     start, end, cat_fld_name,
+                     tot_fld_name, bin_mins,
+                     output_path=output_path,
+                     verbose=1)
