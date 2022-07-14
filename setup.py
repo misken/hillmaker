d2aaccac9a699b065224a28b317e5dcc10846bf8
misken <isken@oakland.edu>
2022-07-14 11:53:15 -0400

    added long description and removed reference to old license

--- setup.py
+++ setup.py
@@ -1,22 +1,17 @@
-# Copyright 2015, 2022 Mark Isken
+# Copyright 2022 Mark Isken
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 from setuptools import find_packages, setup
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(name='hillmaker',
       version='0.4.0',
       description='Occupancy analysis tool for systems having time of day and day of week effects',
+      long_description=long_description,
       author='Mark Isken',
       author_email='isken@oakland.edu',
       url='http://github.com/misken/hillmaker/',
