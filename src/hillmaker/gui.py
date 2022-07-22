4de351bc3ef87a7bc5366c4fc67592182aca8e6a
jwnorm <norman.w.jacob@gmail.com>
2022-07-22 14:39:25 -0400

    fixed issue with .exe file

--- src/hillmaker/gui.py
+++ src/hillmaker/gui.py
@@ -1,16 +1,4 @@
 # Copyright 2022 Mark Isken, Jacob Norman
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 from datetime import datetime as dt
 import os
@@ -164,9 +152,9 @@ def get_user_input(argv=None):
                         action='store',
                         widget='Slider',
                         default=0,
-                        help='The higher the integer, the more verbose the output',
+                        help='0=logging.WARNING, 1=logging.INFO, 2=logging.DEBUG',
                         type=int,
-                        gooey_options={'min': 0, 'max': 5}
+                        gooey_options={'min': 0, 'max': 2}
                         )
 
     args = parser.parse_args()
--- src/hillmaker/hills.py
+++ src/hillmaker/hills.py
@@ -1,18 +1,6 @@
 """Hillmaker"""
 
-# Copyright 2015, 2022 Mark Isken, Jacob Norman
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright 2022 Mark Isken, Jacob Norman
 
 import sys
 from pathlib import Path
@@ -489,7 +477,7 @@ def process_command_line(argv=None):
 
     # Do the parsing and return the populated namespace with the input arg values
     # If argv == None, then ``parse_args`` will use ``sys.argv[1:]``.
-    args = parser.parse_args(argv)
+    args, unknown = parser.parse_known_args(argv)
     return args
 
 def update_args(args, toml_config):
