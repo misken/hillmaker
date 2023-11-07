56c1eb128c9b9570fecb7f22e5bb3511f9601609
misken <isken.mark.w@gmail.com>
2023-11-07 06:43:19 -0500

    added multiple record test

--- tests/test_occ_rectypes.py
+++ tests/test_occ_rectypes.py
@@ -644,6 +644,7 @@ def test_right_mbins_boundary():
 
     assert isclose(bydatetime_df['occupancy'].sum(), (len(occ_series) * 30 + 10) / 30)
 
+
 def test_outer_mbins_boundary():
     # Create test case
 
@@ -684,3 +685,83 @@ def test_outer_mbins_boundary():
         pd.Series([1.0] * len(occ_series)))
 
     assert isclose(bydatetime_df['occupancy'].sum(), (len(occ_series) * 30) / 30)
+
+
+def test_occ_multiple_recs():
+
+    scenario_name = 'multiple_stop_recs'
+    bin_size_minutes = 30
+    start_analysis_dt = pd.Timestamp('2024-01-01')
+    end_analysis_dt = pd.Timestamp('2024-01-01')
+    stop_records = [{'InRoomTS': pd.Timestamp('2023-12-31 11:20'),
+                     'OutRoomTS': pd.Timestamp('2024-02-01 8:50'),
+                     'type': 'outer'},
+                    {'InRoomTS': pd.Timestamp('2024-01-01 7:20'),
+                     'OutRoomTS': pd.Timestamp('2024-01-01 8:50'),
+                     'type': 'inner'},
+                    {'InRoomTS': pd.Timestamp('2024-01-01 23:20'),
+                     'OutRoomTS': pd.Timestamp('2024-02-01 8:30'),
+                     'type': 'right'},
+                    {'InRoomTS': pd.Timestamp('2023-12-31 10:20'),
+                     'OutRoomTS': pd.Timestamp('2024-01-01 7:50'),
+                     'type': 'left'}
+                    ]
+
+    stops_df = pd.DataFrame(stop_records)
+    print(stops_df)
+
+    scenario_params = {'scenario_name': scenario_name,
+                       'data': stops_df,
+                       'in_field': 'InRoomTS', 'out_field': 'OutRoomTS',
+                       'start_analysis_dt': start_analysis_dt,
+                       'end_analysis_dt': end_analysis_dt,
+                       'bin_size_minutes': bin_size_minutes}
+
+    # Create scenario and run hillmaker
+    scenario = create_scenario(scenario_params)
+    scenario.compute_hills_stats()
+    bydatetime_df = scenario.get_bydatetime_df(by_category=False)
+
+    # Arrivals
+    assert bydatetime_df.loc[pd.Timestamp('2024-01-01 7:00')]['arrivals'] == 1.0
+    assert bydatetime_df.loc[pd.Timestamp('2024-01-01 23:00')]['arrivals'] == 1.0
+    assert bydatetime_df['arrivals'].sum() == 2.0
+
+    # Departures
+    assert bydatetime_df.loc[pd.Timestamp('2024-01-01 7:30')]['departures'] == 1.0
+    assert bydatetime_df.loc[pd.Timestamp('2024-01-01 8:30')]['departures'] == 1.0
+    assert bydatetime_df['departures'].sum() == 2.0
+
+    # Occupancy
+
+    # outer
+    outer_occ_series = pd.Series([1.0] * len(bydatetime_df))
+
+    # left
+    left_occ_series = pd.Series([0.0] * len(bydatetime_df))
+    for i in range(15):
+        left_occ_series[i] += 1.0
+    left_occ_series[15] += 20 / 30
+
+    # right
+    right_occ_series = pd.Series([0.0] * len(bydatetime_df))
+    right_occ_series[46] += 10 / 30
+    right_occ_series[47] += 30 / 30
+
+    # inner
+    inner_occ_series = pd.Series([0.0] * len(bydatetime_df))
+    inner_occ_series[14] += 10 / 30
+    inner_occ_series[15] += 30 / 30
+    inner_occ_series[16] += 30 / 30
+    inner_occ_series[17] += 20 / 30
+
+    total_occ_series = pd.Series(outer_occ_series + left_occ_series + right_occ_series + inner_occ_series)
+
+    occ_series = pd.Series(
+        bydatetime_df.loc[pd.Timestamp('2024-01-01 0:00'):pd.Timestamp('2024-01-01 23:30')]['occupancy'])
+    occ_series.reset_index(inplace=True, drop=True)
+
+    pd.testing.assert_series_equal(occ_series, total_occ_series, check_names=False)
+
+
+
